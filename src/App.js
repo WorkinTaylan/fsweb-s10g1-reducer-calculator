9b21b9b9cebe69f081cdc37e9b371902d3bdfd1b
WorkinTaylan <acikgozta@gmail.com>
2023-02-27 17:52:29 +0300

    States have been observed on UI successfully.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CalcButton from './components/CalcButton';
 
 function App() {
   const [state, dispatch]=useReducer(reducer, initialState);
+  //console.log("stateler burda", initialState)
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,10 +17,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
 
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
               <span id="operation"><b>Operation:</b>{state.operation}</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="memory"><b>Memory:</b>{state.memory}</span>
             </div>
 
             <div className="row">
